212cafe845f723615095afb41fa3777cdce1d2c2
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2021-10-17 00:17:12 +0200

    ...

--- js/base.js
+++ js/base.js
@@ -9,3 +9,28 @@ LC_NOTICE_END */
 import "https://code.jquery.com/jquery-3.6.0.min.js"
 
 $("body").append("ciao")
+
+// ================================================================
+//                             TMP
+// ================================================================
+
+function m_openAllDetails()
+{
+    $("details").attr('open', '');
+}
+
+function m_closeAllDetails()
+{
+    $('details').removeAttr('open');
+}
+
+// ================================================================
+//                          LC - export
+// ================================================================
+
+var lc = {
+    openAllDetails : m_openAllDetails,
+    closeAllDetails : m_closeAllDetails
+};
+
+export { lc };
