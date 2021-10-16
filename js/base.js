852d5e58f81631fcb0abad071a591ce6490eecbd
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2021-10-16 10:39:05 +0200

    ...

---
+++ js/base.js
@@ -0,0 +1,11 @@
+/* LC_NOTICE_BEGIN
+LC_NOTICE_END */
+
+// ================================================================
+//                            MODULES
+// ================================================================
+
+// we need jQuery library
+import "https://code.jquery.com/jquery-3.6.0.min.js"
+
+$("body").append("ciao")
